9f9e89b9d27c71cfc89934084e8c446e2120cc40
Adityarya11 <arya050411@gmail.com>
2025-08-31 23:49:55 +0530

    Phase 2: Mongo Tested

--- internal/models/url.go
+++ internal/models/url.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type URL struct {
-	ShortCode   string    `json:"shortCode"`
-	OriginalURL string    `json:"originalUrl"`
-	CreatedAt   time.Time `json:"createdAt"`
-	ExpiresAt   time.Time `json:"expiresAt"`
-	ClickCount  int       `json:"clickCount"`
+	ShortCode   string    `bson:"shortCode" json:"shortCode"`
+	OriginalURL string    `bson:"originalUrl" json:"originalUrl"`
+	CreatedAt   time.Time `bson:"createdAt" json:"createdAt"`
+	ExpiresAt   time.Time `bson:"expiresAt" json:"expiresAt"`
+	ClickCount  int       `bson:"clickCount" json:"clickCount"`
 }
--- internal/repository/mongo.go
+++ internal/repository/mongo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"log"
 	"url-shortener/internal/models"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -20,12 +21,16 @@ func NewMongoRepo(db *mongo.Database, collectionName string) *MongoRepo {
 
 func (r *MongoRepo) Save(url *models.URL) error {
 	_, err := r.collection.InsertOne(context.Background(), url)
-	return err
+	if err != nil {
+		return err
+	}
+	log.Printf("✅ Saved to Mongo: %s -> %s\n", url.ShortCode, url.OriginalURL)
+	return nil
 }
 
 func (r *MongoRepo) Find(shortCode string) (*models.URL, error) {
 	var url models.URL
-	err := r.collection.FindOne(context.Background(), bson.M{"shortcode": shortCode}).Decode(&url)
+	err := r.collection.FindOne(context.Background(), bson.M{"shortCode": shortCode}).Decode(&url)
 	if err != nil {
 		return nil, err
 	}
@@ -35,8 +40,8 @@ func (r *MongoRepo) Find(shortCode string) (*models.URL, error) {
 func (r *MongoRepo) IncrementClicks(shortCode string) error {
 	_, err := r.collection.UpdateOne(
 		context.Background(),
-		bson.M{"shortcode": shortCode},
-		bson.M{"$inc": bson.M{"clickcount": 1}},
+		bson.M{"shortCode": shortCode},
+		bson.M{"$inc": bson.M{"clickCount": 1}},
 	)
 	return err
 }
