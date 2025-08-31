da7ba0bddb568903ce63bf03418923b309a04766
Adityarya11 <arya050411@gmail.com>
2025-08-31 23:28:46 +0530

    Phase 2: Mongo Initialised, and running

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"url-shortener/internal/handlers"
 	"url-shortener/internal/repository"
 	"url-shortener/internal/services"
+	"url-shortener/pkg/database"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Wire dependencies
-	repo := repository.NewMemoryRepo()
-	service := services.NewURLService(repo)
+	// Load environment variables
+	_ = godotenv.Load()
+
+	useMongo := os.Getenv("USE_MONGO")
+
+	// ✅ declare once here
+	var service *services.URLService
+
+	if useMongo == "true" {
+		db := database.ConnectMongo()
+		mongoRepo := repository.NewMongoRepo(db, os.Getenv("MONGO_COLLECTION"))
+		service = services.NewURLService(mongoRepo) // ✅ just assign
+		log.Println("🚀 Running with MongoDB repo")
+	} else {
+		repo := repository.NewMemoryRepo()
+		service = services.NewURLService(repo) // ✅ just assign
+		log.Println("🚀 Running with in-memory repo")
+	}
+
 	handler := &handlers.Handler{Service: service}
 
 	// Setup router
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module url-shortener
 
 go 1.22.5
 
-require go.mongodb.org/mongo-driver v1.17.4
+require (
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver v1.17.4
+)
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
